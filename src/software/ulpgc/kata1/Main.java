bce04490b5088736b89a89ae4e14b37d37eb3ef7
Nahuel Cosme <ncdv.2003@gmail.com>
2023-11-24 11:52:40 +0000

    new Rectangle

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,4 +1,11 @@
 package software.ulpgc.kata1;
 
 public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(3, 5);
+        System.out.println(rectangle);
+        System.out.println(rectangle.getLength());
+        System.out.println(rectangle.getWidth());
+        System.out.println(rectangle.getArea());
+    }
 }
