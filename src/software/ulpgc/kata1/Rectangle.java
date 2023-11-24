47ec5036b97ae7a931b6785b2bf334a5bc0f3063
Nahuel Cosme <ncdv.2003@gmail.com>
2023-11-24 11:50:32 +0000

    Rectangle with constructor

--- src/software/ulpgc/kata1/Rectangle.java
+++ src/software/ulpgc/kata1/Rectangle.java
@@ -1,4 +1,11 @@
 package software.ulpgc.kata1;
 
 public class Rectangle {
+    private final long length;
+    private final long width;
+
+    public Rectangle(long length, long width) {
+        this.length = length;
+        this.width = width;
+    }
 }
